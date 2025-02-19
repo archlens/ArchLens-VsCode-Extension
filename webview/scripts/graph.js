6fd99c792f46df099e7c73c42f83829281d7bb7c
SebastianHylander <hylandersebastian@gmail.com>
2025-02-19 22:42:55 +0100

    made styling to match the original ArchLens

--- webview/scripts/graph.js
+++ webview/scripts/graph.js
@@ -2,26 +2,38 @@ import cytoscape from "../node_modules/cytoscape/dist/cytoscape.esm.mjs";
 const vscode = acquireVsCodeApi();
 
 function make_graph(elements){
+    const font_height = 15;
+    const block_extra_length = 50;
+    const font_width = font_height*.61; // Approx
+    const border_width = 2;
+
     var cy = cytoscape({
         container: document.getElementById('graph'),
         elements: elements,
         style: [
             { selector: 'node', style: {
-                'background-color': '#3498db',
-                'width': '75px', 'height': '50px',
-                'shape': 'roundrectangle', 'text-halign': 'center',
+                'background-color': '#F0FFFF',
+                'width': ((node) => {return (node.data('label').length)*font_width+block_extra_length}), 
+                'height': 40,
+                'shape': 'polygon',
+                'shape-polygon-points': ((node) => {const label_width = node.data('label').length*font_width; const label_ratio = (label_width/(label_width+block_extra_length)); const angle_ratio = (10/(label_width+block_extra_length)); return [-1,-1, label_ratio,-1, (label_ratio+angle_ratio),-.1, -1,-.1, 1,-.1, 1,1, -1,1]}), // Yes
+                'color': '#000000', 
+                'border-width': border_width, 'border-color': '#000000',
                 'label': 'data(label)',
-                'color': '#fff', 'text-valign': 'center',
-                'border-width': 2, 'border-color': '#202020'
+                'text-halign': 'right', 'text-valign': 'top',
+                'text-margin-x': (node) => {return ((node.data('label').length)*font_width*-1 - block_extra_length + 10)},
+                'text-margin-y': font_height+2,
+                'font-family': 'monospace',
+                'font-size': font_height
+
             }},
             { selector: 'edge', style: {
-                'width': 3, 'line-color': '#2c3e50',
-                'target-arrow-shape': 'triangle', 'curve-style': 'bezier',
+                'width': 2, 'line-color': '#000000',
+                'target-arrow-shape': 'triangle-backcurve', 'curve-style': 'bezier',
+                'arrow-scale': 1.50,
                 'label': 'data(label)',
                 'color': '#fff', 'text-valign': 'center',
-            }},
-            { selector: '[type = "special"]', style: {
-                'background-color': 'gold' // Highlights special nodes
+                'font-family': 'monospace'
             }},
 
         ],
