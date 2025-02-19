394a10cbb26e5e998e97b216fba65ee01033b89a
SebastianHylander <hylandersebastian@gmail.com>
2025-02-19 15:33:16 +0100

    added graph.ts for handling graph in vscode

---
+++ archlens-in-vscode/src/utilities/graph.ts
@@ -0,0 +1,73 @@
+interface Graph {
+    modules: {
+        name: string
+        files: string[]
+    }[]
+    files: {
+        name: string
+        module: string
+        edge_to: string[]
+    }[]
+}
+
+class GraphFile {
+    constructor (
+        public name : string,
+        public edge_to : Set<GraphFile>,
+        public module : GraphModule | undefined
+    ) {}
+}
+
+class GraphModule {
+
+    constructor (
+        public name : string,
+        public files : Set<GraphFile>
+    ) {}
+}
+
+export function buildGraph(json :string) : Set<GraphModule>{
+    const graph : Graph = JSON.parse(json)
+    let modules = graph.modules;
+    let files = graph.files;
+
+    let parsedModules = new Map<string, GraphModule>();
+
+    let parsedFiles = new Map<string, GraphFile>();
+
+    let _filenameToEdges = new Map<String, string[]>();
+
+    for (let f of files){
+        let parsedFile = new GraphFile (f.name, new Set, undefined);
+        parsedFiles.set(f.name, parsedFile);
+        _filenameToEdges.set(f.name, f.edge_to);
+    }
+
+    for (let f of files){
+        let k = f.name;
+        let v = parsedFiles.get(k);
+        for(let edge of _filenameToEdges.get(k)!) {
+            v!.edge_to.add(parsedFiles.get(edge)!);
+        }
+        
+        
+    }
+
+    for (let m of modules){
+        let parsedModule = new GraphModule (m.name, new Set);
+        for (let f of m.files) {
+            parsedModule.files.add(parsedFiles.get(f)!);
+            parsedFiles.get(f)!.module = parsedModule;
+        }
+
+        parsedModules.set(m.name, parsedModule);
+    }
+
+    let graphSet = new Set<GraphModule>;
+
+    for (let m of modules) {
+        graphSet.add(parsedModules.get(m.name)!)
+    }
+
+    return graphSet
+}
