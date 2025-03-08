4181e9d63211cee1723ac842d1524fbfa4ba6dad
casperh123 <casho@itu.dk>
2025-03-08 15:37:14 +0100

    Remove redundant .layout() call on cy

--- webview/scripts/graph.js
+++ webview/scripts/graph.js
@@ -18,37 +18,50 @@ function make_graph(elements){
         fit: true,
         padding: 500,
         idealEdgeLength: 1000
-    } 
+    }
 
-    var cy = cytoscape({
+    const cy = cytoscape({
         container: document.getElementById('graph'),
         elements: elements,
         style: [
-            { selector: 'node', style: {
-                'background-color': '#F0FFFF',
-                'width': ((node) => {return getLabelLength(node)*font_width+block_extra_length}), 
-                'height': 40,
-                'shape': 'polygon',
-                'shape-polygon-points': ((node) => {const label_width = getLabelLength(node)*font_width; const label_ratio = (label_width/(label_width+block_extra_length)); const angle_ratio = (10/(label_width+block_extra_length)); return [-1,-1, label_ratio,-1, (label_ratio+angle_ratio),-.1, 1,-.1, 1,1, -1,1, -1,-1, -1,-.1, (label_ratio+angle_ratio),-.1, -1,-.1, -1,-1]}), // Yes
-                'color': '#000000', 
-                'border-width': border_width, 'border-color': '#000000',
-                'border-cap': 'butt',
-                'label': 'data(label)',
-                'text-halign': 'right', 'text-valign': 'top',
-                'text-margin-x': (node) => {return (getLabelLength(node)*font_width*-1 - block_extra_length + 10)},
-                'text-margin-y': font_height+1,
-                'font-family': 'monospace',
-                'font-size': font_height
+            {
+                selector: 'node', style: {
+                    'background-color': '#F0FFFF',
+                    'width': ((node) => {
+                        return getLabelLength(node) * font_width + block_extra_length
+                    }),
+                    'height': 40,
+                    'shape': 'polygon',
+                    'shape-polygon-points': ((node) => {
+                        const label_width = getLabelLength(node) * font_width;
+                        const label_ratio = (label_width / (label_width + block_extra_length));
+                        const angle_ratio = (10 / (label_width + block_extra_length));
+                        return [-1, -1, label_ratio, -1, (label_ratio + angle_ratio), -.1, 1, -.1, 1, 1, -1, 1, -1, -1, -1, -.1, (label_ratio + angle_ratio), -.1, -1, -.1, -1, -1]
+                    }), // Yes
+                    'color': '#000000',
+                    'border-width': border_width, 'border-color': '#000000',
+                    'border-cap': 'butt',
+                    'label': 'data(label)',
+                    'text-halign': 'right', 'text-valign': 'top',
+                    'text-margin-x': (node) => {
+                        return (getLabelLength(node) * font_width * -1 - block_extra_length + 10)
+                    },
+                    'text-margin-y': font_height + 1,
+                    'font-family': 'monospace',
+                    'font-size': font_height
 
-            }},
-            { selector: 'edge', style: {
-                'width': 2, 'line-color': '#000000',
-                'target-arrow-shape': 'triangle-backcurve', 'curve-style': 'bezier',
-                'arrow-scale': 1.50,
-                'label': 'data(label)',
-                'color': '#fff', 'text-valign': 'center',
-                'font-family': 'monospace'
-            }},
+                }
+            },
+            {
+                selector: 'edge', style: {
+                    'width': 2, 'line-color': '#000000',
+                    'target-arrow-shape': 'triangle-backcurve', 'curve-style': 'bezier',
+                    'arrow-scale': 1.50,
+                    'label': 'data(label)',
+                    'color': '#fff', 'text-valign': 'center',
+                    'font-family': 'monospace'
+                }
+            },
 
         ],
         layout: {
@@ -56,11 +69,10 @@ function make_graph(elements){
             fit: true,
             idealEdgeLength: 200
         },
-        
     });
 
-    cy.layout().run()
-
+    cy.run()
+    
     // Add a click event to edges
     cy.on('tap', 'edge', function(evt) {
         var edge = evt.target;
