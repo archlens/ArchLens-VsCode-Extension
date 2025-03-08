6721c7557c0fe7821cf740d1fa00e55f208b4afb
casperh123 <casho@itu.dk>
2025-03-08 17:06:08 +0100

    remove config

--- archlens-in-vscode/src/filesystem/pathResolver.ts
+++ archlens-in-vscode/src/filesystem/pathResolver.ts
@@ -1,17 +1,5 @@
 import * as vscode from 'vscode';
 
-export function graphPath() {
-    const workspaceRootPath = getWorkspaceRoot();
-
-    return vscode.Uri.joinPath(workspaceRootPath!, "/ArchLens/diagrams/modules.json");
-}
-
-export function archLensPath() {
-    const workspaceRootPath = getWorkspaceRoot();
-
-    return vscode.Uri.joinPath(workspaceRootPath!, "/ArchLens/src/cli_interface.py");
-}
-
 function getWorkspaceRoot() {
     return vscode.workspace.workspaceFolders?.[0]?.uri;
 }
